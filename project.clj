8dfb33a87a7105e26b9385c85592709fcb7a3914
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-19 01:46:14 +0800

    Basic irc->gitter function
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject irc-bridge "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure-tw's irc-bot to redirect message to slack/irc/gitter."
+  :url "https://github.com/coldnew/irc-bridge"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [com.taoensso/timbre "4.1.4"]
+                 [org.clojure/data.json "0.2.6"]
+                 [irclj "0.5.0-alpha4"]]
+  :main irc-bridge.core)
--- src/irc_bridge/core.clj
+++ src/irc_bridge/core.clj
@@ -1,6 +1,49 @@
-(ns irc-bridge.core)
+(ns irc-bridge.core
+  (:require [clojure.edn :as edn]
+            [taoensso.timbre :as timbre :refer (debug info warn error fatal)]
+            [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [irclj.core :as irc]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-config
+  "Parse irc-bot config."
+  [url]
+  (edn/read-string (slurp url)))
+
+;; TODO: error handling
+(defn message->gitter
+  "Send message to gitter."
+  [{:keys [rom-id api-key]} message]
+  (client/post (str "https://api.gitter.im/v1/rooms/" rom-id "/chatMessages")
+               {:content-type :json
+                :accept :json
+                :headers {"Authorization" (str "Bearer " api-key)}
+                :conn-timeout (* 10 1000)
+                :body (json/write-str {:text message })})
+  (info (str "message->gitter: " message)))
+
+;; TODO: quit connect safely
+(defn connect-irc
+  [{:keys [gitter irc]}]
+  (let [server   (:server irc)
+        port     (:port irc)
+        nickname (:nickname irc)]
+    (info (str "Connect to " server ":" port ", nickname: " nickname))
+    (irc/connect server port nickname
+                 :callbacks {:privmsg
+                             (fn [irc {:keys [nick text]}]
+                               (message->gitter gitter (str "`ircbot` <" nick ">: " text)))}
+                 )))
+
+(defn create-irc-bot [config]
+  (let [conn (connect-irc config)
+        channel (:channel (:irc config))]
+    (info (str "Join to channel: " channel))
+    (irc/join conn channel)))
+
+(defn -main [& args]
+  (let [arg1 (nth args 0)]
+    (if arg1
+      (-> (parse-config arg1)
+          (create-irc-bot))
+      (fatal "Please specify config file."))))
