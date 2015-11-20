aea5cc8951b37b8422d284297cdb1785ccf1df1b
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-20 09:29:33 +0800

    Add time tick to keep bot alive
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/irc_bridge/core.clj
+++ src/irc_bridge/core.clj
@@ -23,24 +23,45 @@
   (info (str "message->gitter: " message)))
 
 ;; TODO: quit connect safely
+;; http://www.dslreports.com/faq/4798
+;; https://github.com/ormiret/spacebot/blob/master/src/spacebot/core.clj
+;; https://github.com/oskarth/breakfast/blob/9d703b1da9de7bf89fc6f1f4aebce0ac7813122c/src/clj/breakfast/server.clj
 (defn connect-irc
   [{:keys [gitter irc]}]
   (let [server   (:server irc)
         port     (:port irc)
-        nickname (:nickname irc)]
+        nickname (:nickname irc)
+        ssl?     (:ssl? irc)]
     (info (str "Connect to " server ":" port ", nickname: " nickname))
     (irc/connect server port nickname
-                 :auto-reconnect-delay-mins 1 ; reconnect delay after disconnect
-                 :timeout-mins 20 ; socket timeout - length of time to keep socket open when nothing happens
+                 :ssl? ssl?
                  :callbacks {:privmsg
                              (fn [irc {:keys [nick text]}]
-                               (message->gitter gitter (str "`ircbot` <" nick ">: " text)))})))
+                               (try
+                                 (message->gitter gitter (str "`ircbot` <" nick ">: " text))
+                                 ;;(catch java.net.SocketException e
+                                 (catch Throwable e
+                                   (info "SocketException, trying to reconnect in xxx ms")
+                                   ))
+                               )
+                             }
+                 )))
 
 (defn create-irc-bot [config]
   (let [conn (connect-irc config)
         channel (:channel (:irc config))]
+    ;; enable keepalive on the socket, otherwise if it times out we never notice and the bot just hangs
+    ;; (-> @conn :connection :socket (.setKeepAlive true))
     (irc/join conn channel)
-    (info (str "Join to channel: " channel))))
+    (info (str "Join to channel: " channel))
+    ;; tick the irc server to prevent get "Connection reset by peer" error.
+    (while true
+      (do
+        (info "tick")
+        (irc/message conn (:server (:irc config)) "Hi")
+        (Thread/sleep 50000)
+        ))
+    ))
 
 (defn -main [& args]
   (let [arg1 (nth args 0)]
