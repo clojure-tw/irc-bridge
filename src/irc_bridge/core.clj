a92319376d74d7db7231fb9063b6447afd07ced5
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-22 19:15:21 +0800

    drop unuse arguments
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/irc_bridge/core.clj
+++ src/irc_bridge/core.clj
@@ -15,7 +15,7 @@
 
 (defn irc-events-dispatcher
   "irc -> gitter, slack"
-  [{:keys [gitter irc] :as config}]
+  []
   (go-loop [{:keys [nickname message type] :as ch} (<! irc/channel)]
     (when-not (re-find #"gitterbot" nickname)
       (gitter/send-message! (irc->gitter ch)))
@@ -25,23 +25,22 @@
 ;; 2. code block -> send to pastebin like system
 (defn gitter-events-dispatcher
   "gitter -> irc, slack"
-  [{:keys [gitter irc] :as config}]
+  []
   (go-loop [{:keys [nickname message] :as ch} (<! gitter/channel)]
     (when-not (re-find #"ircbot" message)
       ;; since irc can't support multi-line message
       (doseq [msg (gitter->irc ch)]
-        (irc/send-message! :gitter msg))
-      )
+        (irc/send-message! :gitter msg)))
     (recur (<! gitter/channel))))
 
 (defn start-irc-bridge
-  [{:keys [gitter irc] :as config}]
+  [config]
   ;; start listener in thread
-  (gitter/event-listener gitter)
-  (irc/event-listener irc)
+  (gitter/event-listener (:gitter config))
+  (irc/event-listener    (:irc    config))
   ;; start dispatcher for handling events
-  (irc-events-dispatcher    config)
-  (gitter-events-dispatcher config))
+  (irc-events-dispatcher)
+  (gitter-events-dispatcher))
 
 (defn -main [& args]
   (let [arg1 (nth args 0)]
